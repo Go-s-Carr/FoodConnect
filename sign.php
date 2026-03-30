9b3ec2c6304e5fbb973f9c2fa0ed65b7d93d6f63
Go-s-Carr <gggggggaazzaazz@gmail.com>
2026-03-30 18:00:32 +0100

    basic sign in page for testing

--- sign.php
+++ sign.php
@@ -1,3 +1,14 @@
 <?php
 //our php
 // ameni
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>starting off with a bang</h1>
+</body>
+</html>';
