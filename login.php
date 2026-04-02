10c332552b7276518507cbd01c579626a12472af
Go-s-Carr <gggggggaazzaazz@gmail.com>
2026-04-02 04:05:46 +0100

    aaaaaaaahhhhhhh n77eb nmout

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 include("sql_functions.php");
 
-require("functions.php");
+include("functions.php");
 
 if (isset($_POST['email'])&&isset($_POST['password']))
 {
@@ -51,7 +51,30 @@ else
     }
 function login()
 {
-echo'hellow';
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="FoodConnect/css/sing.css">
+    <link rel="stylesheet" href="FoodConnect/css/global.css">
+</head>
+<body class="body">
+    <form action="FoodConnect/login.php" method="post">
+        <fieldset class="window">
+            <legend><h3>Login</h3></legend>
+            <div>
+                <input type="text" placeholder="Email" id="email" name="email">
+                <br>
+                <input type="password" placeholder="password (8 characters)" id="password" name="password">
+                <br>
+                <button type="submit" name="login">Login</button>
+            </div>
+        </fieldset>
+    </form>
+</body>
+</html>';
 }
 
    
