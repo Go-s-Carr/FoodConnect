cd60df48c846995a1ff5bc42551e0623f3f7380f
Go-s-Carr <gggggggaazzaazz@gmail.com>
2026-04-02 04:30:47 +0100

    asd

--- initialize.php
+++ initialize.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "admin";
 $pass = "TOnFlores02:10.";
-$db   = "foodconnect";
+$db   = "account";
 
 // Create connection (without database)
 $conn = new mysqli($host, $user, $pass);
